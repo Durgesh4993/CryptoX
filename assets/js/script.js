d834ea957c61ed96d6c4c5f3c78b6b69ddee04f8
Durgesh kumar prajapati <98798977+Durgesh4993@users.noreply.github.com>
2023-12-30 17:31:50 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,7 +20,6 @@ const addEventOnElem = function (elem, type, callback) {
 const navbar = document.querySelector("[data-navbar]");
 const navbarLinks = document.querySelectorAll("[data-nav-link]");
 const navToggler = document.querySelector("[data-nav-toggler]");
-
 const toggleNavbar = function () {
   navbar.classList.toggle("active");
   navToggler.classList.toggle("active");
